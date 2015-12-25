93bf685e08a268a879e1afad07c234ac57344d82
Kevin Grandon <kevingrandon@yahoo.com>
2015-12-25 11:46:55 -0500

    Listen for bash history changes.

---
+++ monitor.js
@@ -0,0 +1,13 @@
+var fs = require('fs')
+
+function homeDir () {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
+
+fs.watch(homeDir() + '/.bash_history', event => {
+  // Only care about change events
+  if (event !== 'change') {
+  	return
+  }
+  console.log('got event!', event)
+})
