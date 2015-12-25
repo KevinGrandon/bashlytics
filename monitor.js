ffa8648b9cd16f9f31bc685507a97fe4ef431141
Kevin Grandon <kevingrandon@yahoo.com>
2015-12-25 11:49:39 -0500

    Add gitignore and modern-standard.

--- monitor.js
+++ monitor.js
@@ -4,7 +4,7 @@ function homeDir () {
   return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 }
 
-fs.watch(homeDir() + '/.bash_history', event => {
+fs.watch(`${homeDir()}/.bash_history`, event => {
   // Only care about change events
   if (event !== 'change') {
   	return
