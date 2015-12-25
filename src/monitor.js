e014b4337fa02a9f5fe0720c4c87f2249e37e554
Kevin Grandon <kevingrandon@yahoo.com>
2015-12-25 12:06:48 -0500

    Output the 10 most used history commands.

--- src/monitor.js
+++ src/monitor.js
@@ -12,5 +12,33 @@ fs.watch(historyFile, event => {
   debug('got change event')
 
   var content = fs.readFileSync(historyFile, 'utf-8')
-  debug('all log content is:', content)
+    .split('\n')
+
+  // A list of commands and their counts.
+  // Structure: [command, count]
+  var commandCounts = []
+
+  // A mapping of all commands to counts
+  var commandMap = {}
+
+  content.forEach(line => {
+    if (line.startsWith('#')) {
+      return
+    }
+    commandMap[line] = commandMap[line] || 1
+    commandMap[line]++
+  })
+
+  for (var i in commandMap) {
+    commandCounts.push([
+      i,
+      commandMap[i]
+    ])
+  }
+
+  // Sort all commands from most -> least used
+  commandCounts.sort((a, b) => b[1] - a[1])
+
+  // Output the top 10 used commands.
+  console.log('top 10 commands: ', commandCounts.slice(0, 10))
 })
